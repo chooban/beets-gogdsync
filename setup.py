5a04956d70882218f6fc786375111c753aec46a1
Ross Hendry <rhendry@googlemail.com>
2024-05-22 21:47:52 +0100

    Resolved warnings

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     install_requires=["beets>=1.6.0", "plexapi>=4.15.0", "pyyaml>=6"],
     platforms="ALL",
-    packages=["beetsplug.gdplaylists"],
+    packages=["beetsplug.gdplaylists", "beetsplug.gdplaylists.playlists"],
 )
