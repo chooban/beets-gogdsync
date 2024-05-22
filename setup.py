5fa343154db83a8b832daac4e869993075ffecb3
Ross Hendry <rhendry@googlemail.com>
2024-05-22 21:12:54 +0100

    Use a toml

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="beets-gdplaylists",
-    version="0.1.1",
-    description="beets plugin to create Grateful Dead playlists",
-    long_description=open("README.md").read(),
-    author="Ross Hendry ",
-    author_email="rhendry@gmail.com",
-    url="https://github.com/chooban/beets-gogd",
-    license="MIT",
+    install_requires=["beets>=1.6.0", "plexapi>=4.15.0", "pyyaml>=6"],
     platforms="ALL",
     packages=["beetsplug.gdplaylists"],
-    install_requires=[
-        "beets>=1.6.0",
-        "plexapi>=4.15.0",
-        "pyyaml>=6"
-    ],
-    extras_require={
-        "dev": [
-            "musicbrainzngs>=0.7.0"
-        ]
-    },
-    classifiers=[
-        'Topic :: Multimedia :: Sound/Audio',
-        'License :: OSI Approved :: MIT License',
-        'Environment :: Console',
-        'Programming Language :: Python :: 3'
-    ]
 )
